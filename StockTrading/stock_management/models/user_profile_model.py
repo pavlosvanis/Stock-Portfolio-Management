ccf61bfa3f785b8ac6481e12d3d4e63201a80c77
Xiao Nie <nx2004@bu.edu>
2024-12-06 16:35:10 -0500

    data type changed for get_portfolio

--- StockTrading/stock_management/models/user_profile_model.py
+++ StockTrading/stock_management/models/user_profile_model.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Dict, List
+from stock_management.models.stock_model import Stock
 from stock_management.utils.logger import configure_logger
 
 logger = logging.getLogger(__name__)
@@ -31,7 +32,7 @@ class UserProfile:
         self.cash_balance = 0,0
         self.current_stock_holding: Dict[str, int] = {}
 
-    def get_portfolio(self) -> Dict[str, int]:
+    def get_portfolio(self) -> Dict[str, Stock]:
         """
         Retrieves the user's current stock portfolio.
 
