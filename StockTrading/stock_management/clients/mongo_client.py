a9355fa01081742bbb68b11e3d79a1b0b19aa05e
Xiao Nie <nx2004@bu.edu>
2024-12-10 02:31:27 -0500

    clients modified

--- StockTrading/stock_management/clients/mongo_client.py
+++ StockTrading/stock_management/clients/mongo_client.py
@@ -9,7 +9,6 @@ from stock_management.utils.logger import configure_logger
 logger = logging.getLogger(__name__)
 configure_logger(logger)
 
-
 MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
 MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
 
